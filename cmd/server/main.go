f50908a82cedf5489c88bd1a745335a12482c77e
Никита Барышников <super.kronk24@yandex.ru>
2023-08-01 12:34:19 +0300

    Создал HTTP-сервер и обработчик для методов POST, GET

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"github.com/NikitaBarysh/metrics_and_alertinc/internal/server"
+
+	"net/http"
+)
+
+func main() {
+	err := http.ListenAndServe(`:8080`, http.HandlerFunc(server.Router))
+	if err != nil {
+		panic(err)
+	}
+}
