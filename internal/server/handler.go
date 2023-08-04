3f6e58709baa4c539b253a37cf6ced728856bb7a
Никита Барышников <super.kronk24@yandex.ru>
2023-08-04 13:15:28 +0300

    Финальная версия iter1

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -21,31 +21,41 @@ func Router(rw http.ResponseWriter, r *http.Request) {
 func post(rw http.ResponseWriter, r *http.Request) {
 	res := strings.Split(r.URL.Path, "/")
 
+	if len(res) < 5 {
+		http.Error(rw, ",", http.StatusNotFound)
+		return
+	}
+
+	update := res[1]
+	if update != "update" {
+		http.Error(rw, "not update", http.StatusNotFound)
+		return
+	}
+
 	metricType := res[2]
 
 	metricName := res[3]
 
 	metricValue := res[4]
 
-	if metricName == "" {
-		http.Error(rw, "no name", http.StatusNotFound)
-	}
-
 	switch metricType {
 	case "counter":
 		value, err := strconv.ParseInt(metricValue, 10, 64)
 		if err != nil {
 			http.Error(rw, "wrong counter type", http.StatusBadRequest)
+			return
 		}
 		counters[metricName] += value
 	case "gauge":
 		value, err := strconv.ParseFloat(metricValue, 64)
 		if err != nil {
 			http.Error(rw, "wrong gauge type", http.StatusBadRequest)
+			return
 		}
 		gauges[metricName] = value
 	default:
 		http.Error(rw, "unknown metric type", http.StatusNotImplemented)
+		return
 	}
 
 	rw.Header().Set("Content-Type", "text/plain")
