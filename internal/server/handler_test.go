bfac1188d1b37b723c0ebecc03f85daf464c794d
Никита Барышников <super.kronk24@yandex.ru>
2023-08-04 13:12:30 +0300

    Покрыл обработчик сервера тестами

--- internal/server/handler_test.go
+++ internal/server/handler_test.go
@@ -10,30 +10,63 @@ import (
 
 func Test_post(t *testing.T) {
 	type want struct {
-		code        int
-		contentType string
+		code int
+		url  string
 	}
 	tests := []struct {
 		name string
 		want want
 	}{
 		{
-			name: "positive test #1",
+			name: "Test#1, not enough argument in url",
 			want: want{
-				code:        200,
-				contentType: "text/plain",
+				code: http.StatusNotFound,
+				url:  "http://localhost:8080/update/counter/someMetric",
+			},
+		},
+		{
+			name: "Test#2, check if all correct",
+			want: want{
+				code: http.StatusOK,
+				url:  "http://localhost:8080/update/counter/someMetric/527",
+			},
+		}, {
+			name: "Test#3, wrong metric",
+			want: want{
+				code: http.StatusNotImplemented,
+				url:  "http://localhost:8080/update/anytype/someMetric/527",
+			},
+		},
+		{
+			name: "Test#4, wrong value for counter metric",
+			want: want{
+				code: http.StatusBadRequest,
+				url:  "http://localhost:8080/update/counter/someMetric/wrong",
+			},
+		},
+		{
+			name: "Test#5, wrong value for gauge metric",
+			want: want{
+				code: http.StatusBadRequest,
+				url:  "http://localhost:8080/update/gauge/someMetric/wrong",
+			},
+		},
+		{
+			name: "Test#6, not update in url",
+			want: want{
+				code: http.StatusNotFound,
+				url:  "http://localhost:8080/notupdate/gauge/someMetric/527",
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := httptest.NewRequest(http.MethodPost, "http://localhost:8080/", nil)
+			r := httptest.NewRequest(http.MethodPost, tt.want.url, nil)
 			rw := httptest.NewRecorder()
 			post(rw, r)
 
 			res := rw.Result()
 			assert.Equal(t, res.StatusCode, tt.want.code)
-			assert.Equal(t, res.Header.Get("Content-Type"), tt.want.contentType)
 
 		})
 	}
