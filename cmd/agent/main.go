f29e282f6038d85e7a6aa9a7c2a730aa6d432b5e
Никита Барышников <super.kronk24@yandex.ru>
2023-08-05 17:30:16 +0300

    Прохождение теста 2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,29 +1,14 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/NikitaBarysh/metrics_and_alertinc/internal/storage"
 
 	"github.com/NikitaBarysh/metrics_and_alertinc/internal/server"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// Обрабатывем сигналы от системы.
-	termSignal := make(chan os.Signal, 1)
-	signal.Notify(termSignal, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
-
 	CreateMemStorage := storage.CreateMemStorage()
 	MemStorageAction := server.MemStorageAction{CreateMemStorage}
-	go MemStorageAction.Run(ctx)
+	go MemStorageAction.Run()
 
-	sig := <-termSignal
-	fmt.Println(sig.String())
 }
