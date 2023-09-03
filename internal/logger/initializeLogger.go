b720475d549f0ab9aff31085dc75be4d6dcdb865
Никита Барышников <super.kronk24@yandex.ru>
2023-09-03 20:04:27 +0300

    изменил инициализацию логгера

--- internal/logger/initializeLogger.go
+++ internal/logger/initializeLogger.go
@@ -18,13 +18,18 @@ func NewLoggingVar() *LoggingVar {
 }
 
 func (l LoggingVar) Initialize(level string) error {
-	lvl, err := zap.ParseAtomicLevel(level)
+	_, err := zap.ParseAtomicLevel(level)
 	if err != nil {
 		return fmt.Errorf("ошибка при прасинге уровня логера: %w", err)
 	}
-	cfg := zap.NewProductionConfig()
-	cfg.Level = lvl
-
+	//cfg := zap.NewProductionConfig()
+	//cfg.Level = lvl
+	//
+	//zl, err := cfg.Build()
+	//if err != nil {
+	//	return fmt.Errorf("ошибка при билдинге логера: %w", err)
+	//}
+	//l.Log = zl
 	return nil
 }
 
